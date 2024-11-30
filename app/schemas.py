58bead8e205801545b19abd92116e8ee159aefa7
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-11-30 19:34:43 +0500

    *

---
+++ app/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class UploadedFileBase(BaseModel):
+    file_name: str
+    file_url: str
+
+
+class UploadedFileCreate(UploadedFileBase):
+    """ Fayl yuklash uchun ishlatiladigan schema """
+    pass
+
+
+class UploadedFileResponse(UploadedFileBase):
+    """ Javob uchun ishlatiladigan schema """
+    id: int
+
+    class Config:
+        orm_mode = True
