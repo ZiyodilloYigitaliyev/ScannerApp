3b120a3cc184cc9549af8ac8ddffd6aeaf2d0de6
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-01 01:38:52 +0500

    **

--- app/database.py
+++ app/database.py
@@ -5,12 +5,17 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 DATABASE_URL = settings.DATABASE_URL
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# DATABASE_URL dan foydalanib SQLAlchemy engine yaratish
+from sqlalchemy.ext.asyncio import create_async_engine
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# Jadvallarni yaratish
-Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
