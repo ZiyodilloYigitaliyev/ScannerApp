51000d73d583d9769a42c1f0263bd06413f92465
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-01 01:27:04 +0500

    *

--- app/database.py
+++ app/database.py
@@ -9,6 +9,8 @@ DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+# Jadvallarni yaratish
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,13 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
-
 class UploadedFileBase(BaseModel):
     file_name: str
     file_url: str
 
-
 class UploadedFileCreate(UploadedFileBase):
     """ Fayl yuklash uchun ishlatiladigan schema """
     pass
-
 
 class UploadedFileResponse(UploadedFileBase):
     """ Javob uchun ishlatiladigan schema """
