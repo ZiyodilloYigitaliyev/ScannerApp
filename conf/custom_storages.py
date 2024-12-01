b7d61be498a984a8cdc2bcf13d1d1def2ba4c645
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-02 01:29:32 +0500

    *-* update

---
+++ conf/custom_storages.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage' 
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'    
