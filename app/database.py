c2f70b386207e1f3379d59485d4649f9f1704135
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-01 16:42:17 +0500

    *

--- app/database.py
+++ app/database.py
@@ -6,14 +6,7 @@ from .config import settings
 from sqlalchemy.ext.asyncio import create_async_engine
 
 DATABASE_URL = settings.DATABASE_URL
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-# Asinxron SQLAlchemy dvigateli
-engine = create_async_engine(DATABASE_URL, echo=True)
-
-
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
