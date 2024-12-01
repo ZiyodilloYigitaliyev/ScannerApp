88eb0551aec34f9cd10433b371d1e10486724e10
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-01 17:08:51 +0500

    **

--- app/database.py
+++ app/database.py
@@ -1,16 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(DATABASE_URL)  # asyncpg ishlatilmaydi
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)  # asyncpg talab qilinadi
+SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
