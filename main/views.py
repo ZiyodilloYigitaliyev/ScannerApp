0dfc9eaaabb8354797526164247328ac6bd390a1
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-12-01 21:21:03 +0500

    *-*

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ import zipfile
 from io import BytesIO
 from django.core.files.storage import default_storage
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import UploadedFile
