0d6f8ab44478fee57cacac33b2286784d857466d
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-04 13:44:32 -0500

    *-*

--- main/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Question
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ['id', 'text', 'correct_answer', 'answerA', 'answerB', 'answerC', 'answerD']
