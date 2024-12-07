4f42560d450bce6f1db3176fad893699de19af17
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-07 21:37:18 +0500

    Update urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/upload/', UploadImageZipView.as_view(), name='upload_zip'),
+    path('upload/', UploadImageZipView.as_view(), name='upload_zip'),
 ]
 # Static va media fayllarni S3'dan olish
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
