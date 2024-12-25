276d1282a0f1a940c13652ad255241a2f7d27a0d
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-25 06:34:17 -0800

    👍

--- random_quest/models.py
+++ random_quest/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Question(models.Model):
     category = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
     text = models.TextField()
-    options = models.TextField()
-    true_answer = models.CharField(max_length=10, null=True, blank=True)
-    image = models.ImageField(upload_to="question_images/", null=True, blank=True)
-    additionalValue = models.IntegerField(max_length=250)
+    options = models.JSONField()
+    true_answer = models.CharField(max_length=10)
+    image = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.category} - {self.subject}: {self.text[:50]}"
+        return self.text
+
+
+class Data(models.Model):
+    additional_value = models.IntegerField()
+    questions = models.ManyToManyField(Question, related_name="data")
--- random_quest/serializers.py
+++ random_quest/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Question
+from .models import Question, Data
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = ['id', 'category', 'subject', 'text', 'options', 'true_answer', 'image']
+
+
+class DataSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, source='questions')
+
+    class Meta:
+        model = Data
+        fields = ['additional_value', 'questions']
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        data_instance = Data.objects.create(**validated_data)
+
+        for question_data in questions_data:
+            question_instance, _ = Question.objects.get_or_create(**question_data)
+            data_instance.questions.add(question_instance)
+
+        return data_instance
