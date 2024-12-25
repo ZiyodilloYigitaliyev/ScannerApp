ee8e79c751c313f8836a360b13f32ba213233613
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-25 06:00:15 -0800

    💯

--- random_quest/views.py
+++ random_quest/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Question
 from .serializers import QuestionSerializer
 import random
 
-class RandomizedQuestionsView(APIView):
-    def get(self, request):
-        
-        questions = Question.objects.all() 
-        serializer = QuestionSerializer(questions, many=True)
-        data = serializer.data
-        random.shuffle(data)
-        return Response(data)
+class PostDataView(APIView):
+    def post(self, request):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Ma'lumot muvaffaqiyatli saqlandi!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
