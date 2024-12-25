3f1b601ee0ee62e08e68fb4441191fb261a91b0d
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-25 11:16:33 -0800

    💯

--- question/views.py
+++ question/views.py
@@ -12,7 +12,7 @@ class SaveRandomDataView(APIView):
             # Ma'lumotni o'qish
             incoming_data = request.data
             additional_value = incoming_data.get('additionalValue')
-            data_items = incoming_data.get('data', {})
+            data_items = incoming_data.get('data', [])
 
             if not additional_value or not data_items:
                 return Response({'error': 'additionalValue yoki data yo\'q'}, status=status.HTTP_400_BAD_REQUEST)
@@ -21,21 +21,18 @@ class SaveRandomDataView(APIView):
             random_number = self.generate_unique_random_number()
 
             # Ma'lumotlarni random tartibda joylash
-            shuffled_keys = list(data_items.keys())
-            random.shuffle(shuffled_keys)
-            randomized_data = {key: data_items[key] for key in shuffled_keys}
+            randomized_data = random.sample(data_items, len(data_items))
 
             # Bazaga saqlash
             random_data_instance = RandomData(random_number=random_number, data=randomized_data)
             random_data_instance.save()
 
             # true_answer ma'lumotlarini saqlash
-            for key, value in data_items.items():
-                for question in value:
-                    question_id = int(question['text'].split('.')[0])
-                    true_answer = question['true_answer']
-                    true_answer_instance = TrueAnswer(question_id=question_id, true_answer=true_answer)
-                    true_answer_instance.save()
+            for item in data_items:
+                question_id = int(item['text'].split('.')[0])
+                true_answer = item['true_answer']
+                true_answer_instance = TrueAnswer(question_id=question_id, true_answer=true_answer)
+                true_answer_instance.save()
 
             # Javob qaytarish
             serializer = RandomDataSerializer(random_data_instance)
@@ -49,3 +46,5 @@ class SaveRandomDataView(APIView):
             random_number = random.randint(1000, 9999)
             if not RandomData.objects.filter(random_number=random_number).exists():
                 return random_number
+
+
