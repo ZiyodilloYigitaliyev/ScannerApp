6aa1c404bd284ac586bb969f2a6572a2e6033a20
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-25 06:56:33 -0800

    👍

--- random_quest/admin.py
+++ random_quest/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "category", "subject", "text")
-    search_fields = ("category", "subject", "text")
-    list_filter = ("category", "subject")
 
 admin.site.register(Question, Data)
