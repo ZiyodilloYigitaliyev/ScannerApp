204146980800d338e7bedec4742f52146cdb3f7d
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-26 06:13:11 -0800

    👍

--- question/views.py
+++ question/views.py
@@ -10,15 +10,16 @@ class GenerateRandomQuestionsView(APIView):
     def post(self, request):
         try: 
             # print(request.data)
-            questions_data = request.data            
+            questions_data = request.data   
+            additional_value = questions_data.get('additional_value')
+            print(f"additional_value: {additional_value}")         
             # Majburiy va boshqa fanlar
             majburiy_fan_1 = questions_data.get('Majburiy_Fan_1', [])
             majburiy_fan_2 = questions_data.get('Majburiy_Fan_2', [])
             majburiy_fan_3 = questions_data.get('Majburiy_Fan_3', [])
             fan_1 = questions_data.get('Fan_1', [])
             fan_2 = questions_data.get('Fan_2', [])
-            additional_value = questions_data.get('additional_value')
-            print(f"additional_value: {additional_value}")
+            
             final_lists = []
 
             for _ in range(additional_value):
