7a857f3139e43eaa67cfca5cdd4d0ef0acafd1be
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-26 06:54:26 -0800

    💯

--- question/models.py
+++ question/models.py
@@ -6,4 +6,4 @@ class QuestionList(models.Model):
 class Question(models.Model):
     list = models.ForeignKey(QuestionList, related_name='questions', on_delete=models.CASCADE)
     question_id = models.IntegerField()
-    true_answer = models.BooleanField()
+    true_answer = models.CharField(max_length=1)
