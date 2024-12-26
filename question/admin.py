9df66d77c738376c61d29c6af95b7333b01bc475
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-26 02:08:08 -0800

    💯

--- question/admin.py
+++ question/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-# Admin.py yozish
-@admin.register(RandomData)
-class RandomDataAdmin(admin.ModelAdmin):
-    list_display = ('random_number',)
-    search_fields = ('random_number',)
-
-@admin.register(TrueAnswer)
-class TrueAnswerAdmin(admin.ModelAdmin):
-    list_display = ('question_id', 'true_answer')
-    search_fields = ('question_id', 'true_answer')
+admin.site.register([QuestionList, Question])
