30f3e92f83d6c0e12741950824022d1badaf36b9
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-06 05:15:28 -0800

    💯

--- conf/urls.py
+++ conf/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from main.views import ProcessZipFileView
 from question.views import GenerateRandomQuestionsView
-
+from response.views import  download_zip_view
 urlpatterns = [
     path("api/questions", GenerateRandomQuestionsView.as_view()),
     path('admin/', admin.site.urls),
     path('upload/', ProcessZipFileView.as_view(), name='process-zip'),
-    
+    path('download-zip/', download_zip_view, name='download_zip'),
 ]
