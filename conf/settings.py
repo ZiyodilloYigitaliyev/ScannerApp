ed0d79b75e3e4c15fc6c39d639f8d39cc0db4140
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-06 03:33:33 -0800

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -73,7 +73,8 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    'http://localhost:3000',  
+    'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'conf.urls'
