ca9be4b2ef30270ee89a9afcdd012b52d85d02a1
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2025-01-24 14:52:54 +0500

    Updated

--- conf/settings.py
+++ conf/settings.py
@@ -35,15 +35,15 @@ INSTALLED_APPS = [
     'django_celery_beat',
     
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-     ),
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 100,
-#     'DEFAULT_PAGINATION_CLASS': None,
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#      ),
+# #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+# #     'PAGE_SIZE': 100,
+# #     'DEFAULT_PAGINATION_CLASS': None,
+# }
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
@@ -193,7 +193,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'https://scan-app-9206bf041b06.herokuapp.com/',
+    'https://scan-app-9206bf041b06.herokuapp.com',
     'http://localhost:5000',
 ]
 
