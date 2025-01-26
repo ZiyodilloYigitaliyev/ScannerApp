16d27dd3a2de9d0f30d1ae9df7c1537ca1e26d68
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-27 00:35:01 +0300

    Update urls.py

--- question/urls.py
+++ question/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import DeleteAllQuestionsView,  ListQuestionsView, FilterQuestionsView, WordFileProcessorView
+from .views import DeleteAllQuestionsView,  ListQuestionsView, FilterQuestionsView, HTMLFromZipView
 
 urlpatterns = [
-    path('yuklash/', WordFileProcessorView.as_view(), name='upload_questions'),
+    path('yuklash/', HTMLFromZipView.as_view(), name='upload_questions'),
     path('question/', FilterQuestionsView.as_view(), name='filter_question'),
     path('questions/', ListQuestionsView.as_view(), name='list_questions'),
     path('delete-all-questions/', DeleteAllQuestionsView.as_view(), name='delete_all_questions'),
