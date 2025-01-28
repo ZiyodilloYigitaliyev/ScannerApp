ba7aa74d3460a08ed9af190729edadb88e40d64c
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2025-01-28 06:14:38 +0500

    Go

--- question/views.py
+++ question/views.py
@@ -70,7 +70,7 @@ class HTMLFromZipView(APIView):
                     "true_answer": None,
                     "category": category,
                     "subject": subject,
-                    #"images": []  # To hold images for this question
+                    "images": []  # To hold images for this question
                 }
 
             # If the text starts with answer choices (A, B, C, D)
