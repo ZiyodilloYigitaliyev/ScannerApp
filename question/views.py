ca8818bb6c05ee8c11e825bb051dc9b00ff94454
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-31 02:54:08 -0800

    Update views.py

--- question/views.py
+++ question/views.py
@@ -350,7 +350,7 @@ class GenerateRandomQuestionsView(APIView):
 
                             # Result yaratish
                             Result.objects.create(
-                                list=list_id,
+                                list_id=question_list.list_id,
                                 true_answer=true_answer,
                                 order=order,
                             )
