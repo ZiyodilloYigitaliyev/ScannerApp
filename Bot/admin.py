0bdef9acede08fa3f1d228d8acb8576e71448527
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-02-03 00:38:56 -0800

    👍

--- Bot/admin.py
+++ Bot/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import BotUser
+from .models import BotUser, ChannelStats
 
 @admin.register(BotUser)
 class BotUserAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'phone_number', 'created_at')
     search_fields = ('user_id', 'phone_number')
     ordering = ('-created_at',)
+    list_filter = ('created_at',)
+    date_hierarchy = 'created_at'
+
+@admin.register(ChannelStats)
+class ChannelStatsAdmin(admin.ModelAdmin):
+    list_display = ('channel_name', 'member_count', 'updated_at')
+    search_fields = ('channel_name', 'member_count')
+    ordering = ('-updated_at',)
+    list_filter = ('updated_at',)
+    date_hierarchy = 'updated_at'
